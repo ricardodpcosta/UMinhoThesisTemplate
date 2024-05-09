edc3bffd6f67743455bbaf64d22b627f11d9142c
ricardodpcosta <76709201+ricardodpcosta@users.noreply.github.com>
2024-05-09 12:41:14 +0100

    Update

---
+++ makemain.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# compilation of source
+pdflatex main;
+
+# bibtex file names
+bibfiles=(
+      main
+      chapi
+      chapii
+      chapiii
+      chapiv
+      chapv
+      chapvi
+      chapvii
+      chapviii
+      chapix
+      chapx
+);
+
+# compilation of bibliography
+for file in ${bibfiles[@]};do
+      if [[ -f ${file}.aux ]];then
+            bibtex $file;
+      fi;
+done;
+
+# compilation for cross references
+pdflatex main;
+pdflatex main;
+
+#end of file
---
+++ preamble/newsgott/makefont.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# generate tfm files
+ttf2tfm newsgott1.ttf -p T1-WGL4.enc;
+ttf2tfm newsgott2.ttf -p T1-WGL4.enc;
+ttf2tfm newsgott3.ttf -p T1-WGL4.enc;
+ttf2tfm newsgott4.ttf -p T1-WGL4.enc;
+
+# end of file
